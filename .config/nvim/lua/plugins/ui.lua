db3cc67f69ac78a25d3824eaa95634e56e11a92f
Anand Dubey <36227152+anandubey@users.noreply.github.com>
2025-07-20 20:00:57 +0530

    feat:nvim: add lazyvim config

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      render = "wrapped-compact",
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        section_separators = "",
+        component_separators = "",
+      },
+    },
+  },
+}
